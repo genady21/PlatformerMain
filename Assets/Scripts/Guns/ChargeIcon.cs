19cc6072385453cd79829d8dec0baae44c1622e2
Valery <wazgenby@gmail.com>
2024-08-19 03:11:19 +0200

    add jumpGun

---
+++ Assets/Scripts/Guns/ChargeIcon.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargeIcon : MonoBehaviour
+{
+    [SerializeField] private Image _backGround;
+    [SerializeField] private Image _foreGround;
+    [SerializeField] private Text _text;
+
+    public void StartCharge()
+    {
+        _backGround.color = new Color(1f, 1f, 1f, 0.2f);
+        _foreGround.enabled = true;
+        _text.enabled = true;
+    }
+
+    public void StopCharge()
+    {
+        _backGround.color = new Color(1f, 1f, 1f, 1f);
+        _foreGround.enabled = false;
+        _text.enabled = false;
+    }
+
+    public void SetChargeValue(float currentCharge, float maxCharge)
+    {
+        _foreGround.fillAmount = currentCharge / maxCharge;
+        _text.text = Mathf.Ceil(maxCharge - currentCharge).ToString();
+    }
+}
+
---
+++ Assets/Scripts/Guns/JumpGun.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class JumpGun : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _playerRigidbody;
+    [SerializeField] private float _speed=10f;
+    [SerializeField] private Transform _spawn;
+    [SerializeField] private Gun _pistol;
+
+    [SerializeField] private float _maxCharge = 3f;
+    private float _currentCharge;
+    private bool _isCharged;
+
+    [SerializeField] private ChargeIcon _chargeIcon;
+
+    private void Update()
+    {
+        if (_isCharged)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                _playerRigidbody.AddForce(-_spawn.forward * _speed, ForceMode.VelocityChange);
+                _pistol.Shot();
+                _isCharged = false;
+                _currentCharge = 0f;
+                _chargeIcon.StartCharge();
+            }
+        }
+        else
+        {
+            _currentCharge += Time.deltaTime;
+            _chargeIcon.SetChargeValue(_currentCharge,_maxCharge);
+            if (_currentCharge >= _maxCharge)
+            {
+                _isCharged = true;
+                _chargeIcon.StopCharge();
+            }
+        }
+        
+    }
+}
