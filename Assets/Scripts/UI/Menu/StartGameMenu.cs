414e6c9163c5c8a6c6dd75ab9b8d40c456adc5ca
Valery <wazgenby@gmail.com>
2024-08-22 16:35:02 +0200

    add portal and fix

--- Assets/Scripts/UI/Menu/StartGameMenu.cs
+++ Assets/Scripts/UI/Menu/StartGameMenu.cs
@@ -13,9 +13,6 @@ namespace UI.Menu
         [SerializeField] private Button _startButton;
         [SerializeField] private Button _exitButton;
        
-        [SerializeField] private Toggle _toggleSoundStartMenu;
-        [SerializeField] private Slider _sliderSoundqStartMenu;
-        
         [SerializeField] private SoundManager _soundManager;
 
 
@@ -23,23 +20,18 @@ namespace UI.Menu
 
         private void Start()
         {
-            _toggleSoundStartMenu.onValueChanged.AddListener(_soundManager.SetMusicEnabled);
-            _sliderSoundqStartMenu.onValueChanged.AddListener(_soundManager.SetValume);
             _startButton.onClick.AddListener(CloseMenuWindow);
             _exitButton.onClick.AddListener(ExitGame);
         }
 
         private void OnDestroy()
         {
-            _toggleSoundStartMenu.onValueChanged.RemoveListener(_soundManager.SetMusicEnabled);
-            _sliderSoundqStartMenu.onValueChanged.RemoveListener(_soundManager.SetValume);
             _startButton.onClick.RemoveListener(CloseMenuWindow);
             _exitButton.onClick.RemoveListener(ExitGame);
         }
         
         public void CloseMenuWindow()
         {
-            //_menuButton.gameObject.SetActive(true);
             SceneManager.LoadScene(1);
             Time.timeScale = 1f;
         }
